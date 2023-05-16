07e496248d4c568cf133ad917f7a67669b65b2b7
assia hannou <assia.hannou-moussaoui@etu.umontpellier.fr>
2023-05-16 17:26:52 +0200

    update

--- Sprint_3/serveur/make.sh
+++ Sprint_3/serveur/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gcc -c global.c -o global.o
-gcc -c funcServ.c -o funcServ.o
-gcc -c serveur.c -o serveur.o
+gcc -c global.c -o global.o -pthread
+gcc -c funcServ.c -o funcServ.o -pthread
+gcc -c serveur.c -o serveur.o -pthread
 gcc global.o funcServ.o serveur.o -o serveur
