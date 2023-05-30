86dc72ecfbd348e8d7e8df2d286087406c56d544
Nicolas Germani <nicolas.germani.ndv@gmail.com>
2023-05-30 23:55:25 +0200

    suppression zip perso

--- Projet-FAR_6h32/Sprint_3/serveur/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gcc -c global.c -o global.o -pthread
-gcc -c funcServ.c -o funcServ.o -pthread
-gcc -c serveur.c -o serveur.o -pthread
-gcc global.o funcServ.o serveur.o -o serveur
--- Projet-FAR_6h32/replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-	deps = [
-		pkgs.iproute2
-  pkgs.clang_12
-		pkgs.ccls
-		pkgs.gdb
-		pkgs.gnumake
-	];
-}
